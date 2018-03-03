5c1c0e6dde33d95a3ead8901d65d7a4fbd5f5954
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-03 21:53:28 +0100

    create application entry point

---
+++ server/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import bodyParser from 'body-parser';
+
+const app = express();
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(3000, () => {
+  console.log('I am running on port 3000')
+});
+
+export default app;
