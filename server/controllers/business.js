1061ac8ffbe583909fd427a483a744a14af973a5
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-04 04:38:37 +0100

    add controller to handle modifying business profile

--- server/controllers/business.js
+++ server/controllers/business.js
@@ -1,6 +1,7 @@
 /**
  * Controller to handle :
-     Adding a new business
+     Adding a new business,
+     Modify business profile
  */
 
 import Business from '../model/business';
@@ -46,6 +47,36 @@ const businessId = parseInt(req.body.id, 10);
    });
 };
 
-const businessController = { addBusiness };
+const modifyBusinessProfile = (req, res) => {
+    const businessId = parseInt(req.params.id, 10);
+    const arrayOfId = businesses.map((business) => {
+        return business.id;
+      });
+      if (arrayOfId.indexOf(businessId) < 0) {
+        return res.status(404).json({
+          message: 'no business exist with specified id',
+        });
+      }
+      const indexToBeModified = arrayOfId.indexOf(businessId);
+    if (req.body.id) {
+      return res.status(400).json({
+        message: 'Business Id cannot be changed!'
+      });
+    }
+    if (req.body.title) {
+      businesses[indexToBeModified].title = req.body.title;
+    }
+    if (req.body.category) {
+        businesses[indexToBeModified].category = req.body.category;
+    }
+    if (req.body.location) {
+        businesses[indexToBeModified].location = req.body.location;
+    }
+    return res.status(200).json({
+      message: 'profile successfully modified',
+      Now: businesses[indexToBeModified]
+    });
+  };
+const businessController = { addBusiness, modifyBusinessProfile };
 
 export default businessController;
