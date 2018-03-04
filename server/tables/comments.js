dcca6bc855155d890e9eecaab37f83866e40e1c9
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-04 03:08:42 +0100

    create dummy instances of  comments

--- server/tables/comments.js
+++ server/tables/comments.js
@@ -1,1 +1,15 @@
-let comment = "kfekf ewfwf wewe";
+import Comment from '../model/comment';
+
+const Comments = [
+    new Comment(1, '01-02-2018', 'Ayo', 'very very good'),
+    new Comment(1, '11-02-2018', 'John', ' good'),
+    new Comment(2, '21-02-2018', 'Doe', 'excellent'),
+    new Comment(2, '05-02-2018', 'Phil', 'fantabulous'),
+    new Comment(4, '09-02-2018', 'Drey', 'shabalala'),
+    new Comment(5, '21-02-2018', 'Bananbas', 'very very good'),
+    new Comment(3, '10-02-2018', 'Krill', 'very very good'),
+    new Comment(3, '15-02-2018', 'Lukaku', 'very very good'),
+    new Comment(6, '18-02-2018', 'Mourinho', 'very very good'),
+];
+
+export default Comments;
