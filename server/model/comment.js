5b4e14c0531caeeffe2259ea9aa7e06704b1cd9c
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-04 03:31:44 +0100

    refactored comment model

--- server/model/comment.js
+++ server/model/comment.js
@@ -3,38 +3,67 @@
  * @class Comment
  */
 class Comment {
-    constructor(businessId, date, commentor, message){
-      this._businessId = businessId;
-      this._date = date;
-      this._commentor = commentor;
-      this._message = message;
+  /**
+     * Create a Business profile.
+     * @param {number} businessId - The id of business.
+     * @param {string} date - The date of comment.
+     * @param {string} commentor - The name of commentor.
+     * @param {string} message - The message.
+     */
+    constructor(businessId, date, commentor, message) {
+      this.BusinessId = businessId;
+      this.Date = date;
+      this.Commentor = commentor;
+      this.Message = message;
     }
- 
-    get businessId(){
-      return this._businessId;
+ /**
+     * Get the id of business.
+     * @return {number} The id of business.
+     */
+    get businessId() {
+      return this.BusinessId;
     }
+    /** Get the date of comment.
+     * @return {string} The date of comment.
+     */
     get date() {
-      return this._date;
+      return this.Date;
     }
-    get commentor(){
-      return this._commentor
+    /** Get the date of comment.
+     * @return {string} The commentors name
+     */
+    get commentor() {
+      return this.Commentor;
     }
-    get message(){
-      return this._message
+    /** Get the message in comment.
+     * @return {string} The message
+     */
+    get message() {
+      return this.Message;
     }
- 
-    set businessId(id){
-       this._businessId = id;
+    /** Set the date of comment.
+     * @param {number} id - the id of comment
+     */
+    set businessId(id) {
+       this.BusinessId = id;
     }
+    /** Set the date of comment.
+     * @param {string} date - the date of comment
+     */
     set date(date) {
-       this._date = date;
+       this.Date = date;
     }
-    set commentor(theCommentor){
-      this._commentor = theCommentor;
+    /** Set the commentor of comment.
+     * @param {string} theCommentor - the commentor
+     */
+    set commentor(theCommentor) {
+      this.Commentor = theCommentor;
     }
-    set message(comment){
-       this._message = comment;
+    /** Set the message of comment.
+     * @param {string} comment - the message
+     */
+    set message(comment) {
+       this.Message = comment;
     }
   }
  export default Comment;
- 
