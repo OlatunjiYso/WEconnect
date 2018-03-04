212a0f6048539096f3c7880ccbf1335f6e8334f7
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-04 05:21:46 +0100

    map modifyBusiness Controller with router

--- server/routes/business.js
+++ server/routes/business.js
@@ -5,5 +5,6 @@ import businessController from '../controllers/business';
 const businessHandler = express.Router();
 
 businessHandler.post('/', businessController.addCenter);
+businessHandler.put('/:id', businessController.modifyBusinessProfile);
 
 export default businessHandler;
