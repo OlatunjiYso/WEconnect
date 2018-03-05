407acc23386984638257e30fbe33fc52f9df7168
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-05 19:36:31 +0100

    map getComments controller to route

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -4,5 +4,6 @@ import commentController from '../controllers/comments';
 
 const commentHandler = express.Router();
 commentHandler.post('/', commentController.addComment);
+commentHandler.get('/', commentController.getComments);
 
 export default commentHandler;
