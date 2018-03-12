2d5f267faa8bcbdcb97f58632f4cdd9f1759346e
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-13 00:02:44 +0100

    Update category model

--- server/models/category.js
+++ server/models/category.js
@@ -1,13 +1,23 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  var Category = sequelize.define('Category', {
-    categoryName: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
+
+export default (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    CategoryId: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
+    categoryName: {
+      allowNull: false,
+      type: DataTypes.STRING
     }
   });
+  Category.associate = (models) => {
+    Category.hasMany(models.Business, {
+      as: 'businesses',
+      through: 'BusinessCategory',
+      foreignKey: 'categoryId'
+    });
+  };
   return Category;
 };
