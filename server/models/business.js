533a02c837e53bcf4d076591d98910f7524a6a6e
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-12 16:42:43 +0100

    Add business model

---
+++ server/models/business.js
@@ -0,0 +1,32 @@
+
+export default (sequelize, DataTypes) => {
+  const Business = sequelize.define('Business', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
+    title: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true
+    },
+    slogan: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true
+    },
+  });
+    Business.associate = (models) => {
+      Business.belongsTo(models.User, { foreignKey: 'userId' });
+      Business.hasMany(models.Review, { foreignKey: 'businessId' });
+      Business.hasOne(models.BusinessPic, { foreignKey: 'businessId' });
+      Business.hasOne(models.Address, { foreignKey: 'businessId' });
+      Business.hasOne(models.About, { foreignKey: 'businessId' });
+      Business.hasOne(models.Hotline, { foreignKey: 'businessId' });
+      Business.hasOne(models.Email, { foreignKey: 'businessId' });
+      Business.hasOne(models.AboutCeo, { foreignKey: 'businessId' });
+    };
+  return Business;
+};
