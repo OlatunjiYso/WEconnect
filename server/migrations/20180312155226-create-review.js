b09f33b4e43c678ec9d4b76fd490b0ed6a90afca
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-12 17:16:25 +0100

    Add Review migration file

---
+++ server/migrations/20180312155226-create-review.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      businessId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Business',
+          key: 'id'
+        }
+      },
+      message: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      reviwerName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }),
+  down: queryInterface =>
+   queryInterface.dropTable('Reviews')
+};
