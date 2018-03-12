ed726512e05e0d773d7d68487539e3aef1cfcbbb
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-12 14:44:37 +0100

    Add userPic migration file

---
+++ server/migrations/20180312125315-create-user-pic.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+     queryInterface.createTable('User_Pics', {
+      userId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      img_path: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }),
+   down: queryInterface =>
+   queryInterface.dropTable('User_Pics')
+};
