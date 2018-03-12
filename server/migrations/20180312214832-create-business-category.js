3c35d7995db53e20f32e7b15bed42077dcf53cb4
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-12 23:10:58 +0100

    Add businessCategory join table and migration file

---
+++ server/migrations/20180312214832-create-business-category.js
@@ -0,0 +1,38 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+   queryInterface.createTable('BusinessCategories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Categories',
+            key: 'CategoryId'
+        },
+      },
+      businessId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Businesses',
+            key: 'id'
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }),
+
+  down: queryInterface =>
+     queryInterface.dropTable('BusinessCategories')
+};
