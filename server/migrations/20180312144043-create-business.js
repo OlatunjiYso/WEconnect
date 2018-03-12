c817f4b45380b6aadffc4c438d2772ef0929cc95
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-12 16:47:24 +0100

    Add business migration file

---
+++ server/migrations/20180312144043-create-business.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+     queryInterface.createTable('Businesses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      slogan: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'User',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }),
+  down: queryInterface =>
+     queryInterface.dropTable('Businesses')
+};
