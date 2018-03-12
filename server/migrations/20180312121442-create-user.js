5c7c207034592b6cbeac12eb975d0909cbe5ba84
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-12 13:50:27 +0100

    Add user migration file

---
+++ server/migrations/20180312121442-create-user.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+      queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      firstname: {
+        type: Sequelize.STRING
+      },
+      lastname: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }),
+
+  down: queryInterface => queryInterface.dropTable('Users')
+
+};
