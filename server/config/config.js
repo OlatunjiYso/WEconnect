1242b3f2d1fa0ba8e1126eff74fe419f7bcb4e38
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-14 09:11:58 +0100

    Make use of environmental variables in db configuration

---
+++ server/config/config.js
@@ -0,0 +1,26 @@
+
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DEVELOPMENT_DB,
+    host: process.env.LOCAL_HOST,
+    dialect: process.env.DB_DIALECT
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.TEST_DB,
+    host: process.env.LOCAL_HOST,
+    dialect: process.env.DB_DIALECT
+  },
+production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.TEST_DB,
+    host: process.env.LOCAL_HOST,
+    dialect: process.env.DB_DIALECT
+  },
+}
