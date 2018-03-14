9aceeb91e9b63df5d61385279dfff289611b0049
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-14 13:58:27 +0100

    Add authorization middleware for business ownership

---
+++ server/middleware/authorization.js
@@ -0,0 +1,56 @@
+import jwt from 'jsonwebtoken';
+
+import db from '../models';
+
+const { Business } = db;
+
+/**
+ * Checks if a user that is about to perform an action on a business is the owner
+ * @param {Object} req -api request
+ * @param {Object} res -api response
+ * @param {Object} next -pass on to next handler
+ * @return {undefined}
+ */
+export default function confirmOwnership(req, res, next) {
+    const token = req.body.token || req.headers.token;
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        req.user = decoded;
+        next();
+    });
+
+    if (req.user.role !== 'businessOwner') {
+        res.status(401)
+            .send({
+                message: 'You are not authorized to perform such action on this business'
+            });
+    } else {
+        // find the actuall owner
+        Business
+            .findOne({
+                where: {
+                    id: req.params.businessId
+                }
+            })
+            .then((business) => {
+                if (!business) {
+                    res.status(400)
+                        .send({
+                            message: 'no such business exists'
+                        });
+                }
+                if (req.user.id !== business.ownerId) {
+                    res.status(401)
+                        .send({
+                            message: 'You are not authorized to perform such action on this business'
+                        });
+                }
+                return next();
+            })
+            .catch((err) => {
+                res.status(500)
+                    .send({
+                        messsage: err.message
+                    });
+            });
+    }
+}
