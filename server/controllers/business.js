9f3c4a1690ab895b97f87faa2694f158adc36fb9
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-15 01:04:13 +0100

    Add controller to create business using real data

--- server/controllers/business.js
+++ server/controllers/business.js
@@ -1,154 +1,56 @@
+import db from '../models';
+
+const { Business } = db;
+
 /**
- * Controllers to handle :
+ * Business Controllers to handle :
             Adding a new business,
-            Modifying business profile
-            Deleting a business,
             Getting a business,
             Getting all business
+            Modifying business profile
+            Deleting a business
             Getting business by category
             Getting business by location
  */
+class businessController {
+  /**
+     * Creates a new business
+     * @param {Object} req -the api request
+     * @param {Object} res -the api response
+     * @return {json} message key
+     */
+    static createBusiness(req, res) {
+       Business
+        .create({
+          ownerId: req.user.id,
+          title: req.body.title,
+          slogan: req.body.slogan || null,
+          overview: req.body.overview || null,
+          email: req.body.email,
+          website: req.body.website || null,
+          phone1: req.body.phone1,
+          phone2: req.body.phone2 || null,
+          facebook: req.body.facebook || null,
+          tweeter: req.body.tweeter || null,
+          image1: req.body.image1 || null,
+          image2: null,
+          image3: null,
+          image4: null,
 
-import Business from '../model/business';
-
-import businesses from '../tables/businesses';
-
-const addBusiness = (req, res) => {
-  const arrayOfId = businesses.map(business => business.id);
-const businessId = parseInt(req.body.id, 10);
-  if (arrayOfId.indexOf(businessId) !== -1) {
-     return res.status(400).json({
-      message: 'A business with this Id already exist, please choose another Id'
-    });
-  }
-  if (!req.body.id || req.body.id.trim().length === 0) {
-    return res.status(400).json({
-      message: 'businessId cannot be empty, please Input Id.'
-    });
-  }
-  if (!req.body.title || req.body.title.trim().length === 0) {
-    return res.status(400).json({
-      message: 'business title cannot be empty, please Input business title.'
-    });
-  }
-  if (!req.body.category || req.body.category.trim().length === 0) {
-    return res.status(400).json({
-      message: 'business category cannot be empty, please Input business category.'
-    });
-  }
-  if (!req.body.location || req.body.location.trim().length === 0) {
-    return res.status(400).json({
-      message: 'business location cannot be empty, please Input business location.'
-    });
-  }
-  const id = parseInt(req.body.id, 10);
-  const newBusiness = new Business(id, req.body.title, req.body.category, req.body.location);
-  businesses.push(newBusiness);
-  res.status(201).json({
-     message: 'business created successfully',
-     businesses
-   });
-};
-
-const modifyBusinessProfile = (req, res) => {
-    const businessId = parseInt(req.params.id, 10);
-    const arrayOfId = businesses.map(business => business.id);
-      if (arrayOfId.indexOf(businessId) < 0) {
-        return res.status(404).json({
-          message: 'no business exist with specified id',
+        })
+        .then((business) => {
+          res.status(201)
+            .send({
+              message: 'business successfully created',
+              business
+            });
+        })
+        .catch((err) => {
+          res.status(400)
+            .send({
+              message: err.errors ? err.errors : err.message
+            });
         });
-      }
-      const indexToBeModified = arrayOfId.indexOf(businessId);
-    if (req.body.id) {
-      return res.status(400).json({
-        message: 'Business Id cannot be changed!'
-      });
     }
-    if (req.body.title) {
-      businesses[indexToBeModified].title = req.body.title;
-    }
-    if (req.body.category) {
-        businesses[indexToBeModified].category = req.body.category;
-    }
-    if (req.body.location) {
-        businesses[indexToBeModified].location = req.body.location;
-    }
-    return res.status(200).json({
-      message: 'profile successfully modified',
-      Now: businesses[indexToBeModified]
-    });
-  };
 
-  const deleteBusiness = (req, res) => {
-    const businessId = parseInt(req.params.id, 10);
-    const arrayOfId = businesses.map(business => business.id);
-    if (arrayOfId.indexOf(businessId) < 0) {
-      return res.status(404).json({
-        message: 'profile with specified id not found'
-      });
-    }
-    const indexToBePopped = arrayOfId.indexOf(businessId);
-    businesses.splice(indexToBePopped, 1);
-    return res.status(200).json({
-      message: 'profile successfully deleted'
-    });
-  };
-
-  const getBusinesses = (req, res) => {
-      if (req.query.location) {
-        const desiredLocation = req.query.location;
-        const matches = businesses.filter(business => business.location === desiredLocation);
-        if (matches.length === 0) {
-          return res.status(200).json({
-            message: 'No matching business'
-          });
-        }
-        return res.status(200).json({
-          Business: matches
-        });
-      }
-      if (req.query.category) {
-        const desiredCategory = req.query.category;
-        const matches = businesses.filter(business => business.category === desiredCategory);
-        if (matches.length === 0) {
-          return res.status(200).json({
-            message: 'No matching business'
-          });
-        }
-        return res.status(200).json({
-          Business: matches
-        });
-      }
-    if (businesses.length === 0) {
-      return res.status(200).json({
-        message: 'No Businesses yet'
-      });
-    }
-    return res.status(200).json({
-      businesses
-    });
-  };
-
-  const getBusiness = (req, res) => {
-    const businessId = parseInt(req.params.id, 10);
-    const arrayOfId = businesses.map(business => business.id);
-    if (arrayOfId.indexOf(businessId) < 0) {
-      return res.status(404).json({
-        message: 'profile with specified id not found'
-      });
-    }
-    const profile = businesses.filter(business => business.id === businessId);
-    return res.status(200).json({
-      profile
-    });
-  };
-
-const businessController = {
-      addBusiness,
-      modifyBusinessProfile,
-      deleteBusiness,
-      getBusinesses,
-      getBusiness
-    };
-
-  export default businessController;
+}
