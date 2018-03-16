131f9a465bdd266e099fe9df983fe55d601c56d9
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-16 11:31:05 +0100

    Update business table

--- server/tables/businesses.js
+++ server/tables/businesses.js
@@ -1,15 +1,56 @@
 import Business from '../model/business';
 
 const Businesses = [
-    new Business(1, 'Super dance', 'Entertainment', 'Lagos'),
-    new Business(2, 'Just suits', 'Fashion', 'Abuja'),
-    new Business(3, 'Delicious', 'Catering', 'Ondo'),
-    new Business(4, 'Cloth Shop', 'Fashion', 'Kwara'),
-    new Business(5, 'Jacket Shop', 'Fashion', 'Anthony'),
-    new Business(6, 'Maize and Corn', 'Agriculture', 'ketu'),
-    new Business(7, 'Clicks', 'Photography', 'Yaba'),
-    new Business(8, 'superStar', 'Technology', 'Ikeja'),
-    new Business(9, 'Cloth Shop', 'Fashion', 'Abuja'),
+    new Business(
+        1,
+        'Super dance',
+        'Entertainment',
+        'Lagos',
+        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo',
+        '080111111',
+        'superdance@andela.com',
+        1
+    ),
+    new Business(
+        2,
+        'Just suits',
+        'Fashion',
+        'Abuja',
+        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo',
+        '0802222222',
+        'justsuits@andela.com',
+        2
+    ),
+    new Business(
+        3,
+        'Delicious',
+        'Catering',
+        'Ondo',
+        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo',
+        '080333333',
+        'deli@andela.com',
+        3
+    ),
+    new Business(
+        4,
+        'Cloth Shop',
+        'Fashion',
+        'Kwara',
+        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo',
+        '08044444444',
+        'cloth@andela.com',
+        '1'
+    ),
+    new Business(
+        5,
+        'Jacket Shop',
+        'Fashion', 'Anthony',
+        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo',
+        '080555555',
+        'somebody@andela.com',
+        5
+    )
+
 ];
 
 export default Businesses;
