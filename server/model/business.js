39e4cfd67a898ed334f39f172d8aba9af911327b
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-16 11:17:34 +0100

    Update business model to have Email, ownerId, Phone and Description.

--- server/model/business.js
+++ server/model/business.js
@@ -9,12 +9,20 @@
      * @param {string} title - The title of business.
      * @param {string} category - The category of business.
      * @param {string} location - The location.
+     * @param {string} description - The description of business.
+     * @param {string} phone - The phone number of business
+     * @param {string} email - The email address of business
+     * @param {number} ownerId - The id of the business owner
      */
-   constructor(id, title, category, location) {
+   constructor(id, title, category, location, description, phone, email, ownerId) {
      this.Id = id;
      this.Title = title;
      this.Category = category;
      this.Location = location;
+     this.Description = description;
+     this.Phone = phone;
+     this.Email = email;
+     this.OwnerId = ownerId;
    }
     /**
      * Get the id of business.
@@ -38,13 +46,34 @@
      return this.Category;
    }
    /**
+     * Get the description of business.
+     * @return {string} The description of business.
+     */
+    get description() {
+      return this.Description;
+    }
+    /**
+     * Get the Phone number of business.
+     * @return {string} The Phone number of business.
+     */
+   get phone() {
+    return this.Phone;
+  }
+  /**
+     * Get the email of business.
+     * @return {string} The email of business.
+     */
+    get email() {
+      return this.Email;
+    }
+    /**
      * Get the location of business.
      * @return {string} The location of business.
      */
    get location() {
-     return this.Location;
-   }
-   /**
+    return this.Location;
+  }
+  /**
      * Set the title of business.
      * @param{string} newTitle -The new title
      */
@@ -65,6 +94,27 @@
    set location(location) {
       this.Location = location;
    }
+   /**
+     * Set the description of business.
+     * @param{string} newDescription -The new description
+     */
+    set description(newDescription) {
+      this.Description = newDescription;
+   }
+   /**
+     * Set the phone number of business.
+     * @param{string} newPhone -The new phone number
+     */
+    set phone(newPhone) {
+      this.Phone = newPhone;
+   }
+   /**
+     * Set the email of business.
+     * @param{string} newEmail -The new email
+     */
+    set email(newEmail) {
+      this.Email = newEmail;
+   }
 /**
      * Get the details of business.
      * @return{object} details -The details
@@ -72,8 +122,10 @@
    get details() {
      const message = {
          title: this.title,
-         category: this.category,
          location: this.location,
+         email: this.email,
+         Hotline: this.phone,
+         Overview: this.description,
          comments: this.comments
      };
      return message;
