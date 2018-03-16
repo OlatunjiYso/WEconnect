6328e4db74b7cd0c759a509939f8886813950f2d
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-16 11:52:05 +0100

    Add user table

---
+++ server/tables/users.js
@@ -0,0 +1,12 @@
+// (userId, username, password, phone)
+
+import User from '../model/user';
+
+const Users = [
+    new User(1, 'olaaz', 'olaaz', '0809999999'),
+    new User(2, 'johndoe', 'johndoe', ' 08088888888'),
+    new User(3, 'whales', 'whales', '0807777777'),
+    new User(4, 'Philss', 'Philss', '0806666666'),
+];
+
+export default Users;
