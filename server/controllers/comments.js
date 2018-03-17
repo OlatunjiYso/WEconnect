7667fa9614132c22b770baae50bdea33e0655b5e
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-17 13:13:59 +0100

    Update review controller

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -10,26 +10,6 @@ import comments from '../tables/comments';
 
 
 const addComment = (req, res) => {
-    if (!req.body.businessId || req.body.businessId.trim().length === 0) {
-        return res.status(400).json({
-            message: 'businessId cannot be empty, please Input businessId.'
-        });
-    }
-    if (!req.body.commentor || req.body.commentor.trim().length === 0) {
-        return res.status(400).json({
-            message: 'commentor field cannot be empty, please Input commentor field.'
-        });
-    }
-    if (!req.body.message || req.body.message.trim().length === 0) {
-        return res.status(400).json({
-            message: 'message cannot be empty, please Input message.'
-        });
-    }
-    if (!req.body.date || req.body.date.trim().length === 0) {
-        return res.status(400).json({
-            message: 'date cannot be empty.'
-        });
-    }
     const businessId = parseInt(req.params.businessId, 10);
     const newComment = new Comment(businessId, req.body.date, req.body.commentor, req.body.message);
     comments.push(newComment);
