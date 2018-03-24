eebbc784c77678545fb6a8706b5db5d30a572200
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-24 10:32:40 +0100

    Add tests for all models

---
+++ server/test/testdata.js
@@ -0,0 +1,71 @@
+const testData = {
+    businessDetails: {
+        ownerId: 1,
+        title: 'Just Suits',
+        slogan: 'clothing you since th 80s',
+        overview: 'Lorem ipsum praga lambda serus lorem ipsum rewund gereb berd ighus',
+        category: 'fashion',
+        email: 'olat@andela.com',
+        location: 'Lagos',
+        address: 'No 25 Anthony Bustop, Ilupeju',
+        phone: '08012345676',
+        whatsapp: '090123456',
+        facebook: 'justasuit.facebook.com',
+        twitter: '@just suit',
+        image: 'image1.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    businessDetail1: {
+        ownerId: 1,
+        title: 'Just Suitss',
+        slogan: 'clothing you since th 80ss',
+        overview: 'Lorem ipsum praga lambda serus lorem ipsum rewund gereb berd ighuss',
+        category: 'fashions',
+        email: 'olat@andela.coms',
+        location: 'Lagoss',
+        address: 'No 25 Anthony Bustop, Ilupejus',
+        phone: '080123456761',
+        whatsapp: '0901234561',
+        facebook: 'justasuit.facebook.coms',
+        twitter: '@just suits',
+        image: 'image1.jpgs',
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    businessDetail2: {
+        ownerId: 1,
+        title: 'Just Ties',
+        slogan: 'clothing you since th 80ss',
+        overview: 'Lorem ipsum praga lambda serus lorem ipsum rewund gereb berd ighuss',
+        category: 'fashions',
+        email: 'tieolat@andela.coms',
+        location: 'Lagos',
+        address: 'No 25 Anthony Bustop, Ilupejus',
+        phone: '0801234567611',
+        whatsapp: '09012345611',
+        facebook: 'justasuit.facebook.coms',
+        twitter: '@just suits',
+        image: 'image2.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+   userDetails: {
+        username: 'wonderguy',
+        password: 'wonderguy',
+        email: 'wonder@gmail.com',
+        createdAt: new Date(),
+        updatedAt: new Date()
+
+    },
+    reviewDetails: {
+        businessId: 1,
+        description: 'wonderful business',
+        reviewerId: '1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+
+    }
+};
+
+export default testData;
