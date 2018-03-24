8a1502905fe2a7b5848b4efa9f5798b14d1229cc
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-24 19:04:39 +0100

    re-arrange routes

--- server/routes/home.js
+++ server/routes/home.js
@@ -2,16 +2,16 @@ import express from 'express';
 
 const home = express.Router();
 
-home.get('*', (req, res) => res.status(404)
-        .json({
-            message: 'Page not found'
-        }));
+home.get('/', (req, res) => {
+    return res.status(404)
+    .json({
+        message: 'Weconnect home'
+    });
+});
 
-    home.get('/', (req, res) => {
-            return res.status(404)
-            .json({
-                message: 'Weconnect home'
-            });
-        });
+home.get('*', (req, res) => res.status(404)
+.json({
+    message: 'Page not found'
+}));
 
 export default home;
