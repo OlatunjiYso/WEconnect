7cd11b98f002ab497f9bbf7c432cf7bd5048f785
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-03-24 10:40:05 +0100

    Modify user controllers and routes to use real data

---
+++ server/routes/user.js
@@ -0,0 +1,29 @@
+import express from 'express';
+
+import userController from '../controllers/user';
+import validator from '../middleware/validations';
+import authenticate from '../middleware/authentication';
+
+const userHandler = express.Router();
+
+userHandler.post(
+    '/signup',
+    validator.checkUsernameExistence,
+    validator.checkEmailExistence,
+    validator.validateSignup,
+    userController.createUser
+);
+
+userHandler.post(
+    '/login',
+    validator.validatelogin,
+    userController.login
+);
+
+userHandler.get(
+    '/myBusiness',
+    authenticate,
+    userController.getMyBusinesses
+);
+
+export default userHandler;
