e520ccbfe7c0abd6243612d1194fd07593715a5e
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-04-08 00:36:57 +0100

    Remove redundant scripts

--- template/public/js/init.js
+++ template/public/js/init.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-    // slider
-      $('.slider').slider();
-
      // modal
       $('.modal').modal();
 
    // select initiation
       $('select').material_select();
     });
+
     // collaspse navbar initiator
     $(".button-collapse").sideNav();
