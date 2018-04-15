247c9a67f3beab000ced8909cb9cf9cd04b5bc5a
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-04-15 22:31:07 +0100

    Add signup container

--- client/src/containers/signup.jsx
+++ client/src/containers/signup.jsx
@@ -14,55 +14,83 @@ import hero from '../assets/images/profession.jpg';
  * @extends {React.Component}
  */
 class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userDetail: {
+                username: '',
+                password: '',
+                email: '',
+                firstname: '',
+                lastname: ''
+            },
+            submitted: false
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount() {
+
+    }
+
+    /** 
+    *
+    *
+    * @returns {func} funtion
+    * 
+    * @memberof SignupForm Component
+    */
+    handleChange(event) {
+        const name = event.target.name;
+        const value = event.target.value
+        this.setState({
+            ...this.state,
+            userDetail: { [name]: value },
+            submitted: true
+        });
+    }
+
+    /** 
+    *
+    *
+    * @returns {func} funtion
+    * 
+    * @memberof Signup Component
+    */
+    handleSubmit(event) {
+        alert('A new signup has been made: ' + this.state.userDetail.firstname);
+        event.preventDefault();
+    }
+
     /** 
     *
     *
     * @returns {JSX} JSX
     * 
-    * @memberof SignupComponent
+    * @memberof Signup Component
     */
     render() {
         return (
             <div>
                 <Navbar />
                 <main>
-                    <div className="row head-font">
-                        <div className="col s4 offset-s4 center-align">
-                            <h2 className="center head-font pink-text"> Sign Up </h2>
-                        </div>
-                    </div>
-                    <div className="row top-pad body-font">
-                        <div className="row">
-                            <div className="col s8 offset-s2 m6 offset-m4 l4 offset-l4">
-                                <form className="pink lighten-5 form-body" id="short-forms">
-                                    <div className="row black-text">
-                                        <label className="input-field col s12">
-                                            <span class="blue-grey-text text-darken-4">Username:</span>
-                                            <input type="text" name="username" />
-                                        </label>
-                                    </div>
-                                    <div className="row black-text">
-                                        <label className="input-field col s12">
-                                            <span class="blue-grey-text text-darken-4">Email:</span>
-                                            <input type="email" name="email" />
-                                        </label>
-                                    </div>
-                                    <div className="row black-text">
-                                        <label className="input-field col s12">
-                                            <span class="blue-grey-text text-darken-4">Password:</span>
-                                            <input type="password" name="password" />
-                                        </label>
-                                    </div>
-                                </form>
-                                <div className="center-align">
-                                    <Link to="all_businesses.html"
-                                        className="waves-effect waves-light pink-text text-lighten-4 blue-grey darken-4  btn top-gap">Sign Up</Link>
-                                </div>
-                                <p className="black-text bottom-pad">
-                                    <em> Not yet Registered? Click
-                        <Link to="signin.html" className="pink-text"> here </Link> to sign in right away</em>
-                                </p>
-                            </div>
+                    <div className="row head-font top-pad-much no-bottom-gap">
+                        <div className="col s8 offset-s2 m6 offset-m3 grees form-jacket">
+                            <h4 className="center head-font form-heading"> Join WEconnect </h4>
+                            <form onSubmit={this.handleSubmit}>
+                                <label className="form-label">Firstname: </label>
+                                <input type="text" value={this.state.userDetail.firstname} onChange={this.handleChange} name="firstname" className="form-input white" />
+                                <label className="form-label">Lastname: </label>
+                                <input type="text" value={this.state.userDetail.lastname} onChange={this.handleChange} name="lastname" className="form-input white" />
+                                <label className="form-label">Email: </label>
+                                <input type="email" value={this.state.userDetail.email} onChange={this.handleChange} name="email" className="form-input white" />
+                                <label className="form-label">Username: </label>
+                                <input type="text" value={this.state.userDetail.username} onChange={this.handleChange} name="username" className="form-input white" />
+                                <label className="form-label"> Password: </label>
+                                <input type="password" value={this.state.userDetail.password} onChange={this.handleChange} name="password" className="form-input white" />
+                                <input type="submit" value="Submit" className="form-btn" />
+                            </form>
                         </div>
                     </div>
                 </main>
