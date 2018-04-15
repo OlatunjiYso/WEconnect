e48b5c49d53611e5f2eab0bb6ec91aad2f400701
OlatunjiYSO <olatunjiyso@yahoo.com>
2018-04-15 22:33:13 +0100

    Refactor entry point

--- client/src/app.js
+++ client/src/app.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, {Component } from 'react';
 
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter } from 'react-router-dom';
+import { Switch, Router, Route } from 'react-router-dom';
 
 import Home from './containers/home';
 import Login from './containers/login';
 import Signup from './containers/signup';
 import UserProfile from './containers/user_profile';
+import BusinessProfile from './containers/business_profile';
 import AllBusinesses from './containers/all_businesses';
+import BusinessRegForm from './containers/business_reg_form';
+import BusinessUpdateForm from './containers/business_update_form';
+import ManageBusiness from './containers/manage_business';
 
-ReactDOM.render(
-<BrowserRouter>
-        <AllBusinesses />
-</BrowserRouter>,
-document.getElementById('app')
-);
+/**
+ * @class App
+ * 
+ * @extends {React.Component}
+ */
+class App extends Component {
+
+        /** 
+            *
+            *
+            * @returns {JSX} JSX
+            * 
+            * @memberof SignupComponent
+            */
+        render() {
+                return (
+                        <div>
+                                <Route exact= {true} path="/" component = { Signup } /> 
+                                <Route path="/login" component = { Login } />  
+                                <Route path="/signup" component = { Signup } />
+                                <Route path="/user/profile" component = { UserProfile } />
+                                <Route path="/business/profile" component = { BusinessProfile } />
+                                <Route path="/businesses" component = { AllBusinesses } />
+                                <Route path="/business/registration" component = { BusinessRegForm } />
+                                <Route path="/business/update" component = { BusinessUpdateForm } />
+                                <Route path="/business/manage" component = { ManageBusiness } />
+                        </div>
+                )
+
+        }
+}
+
+export default App;
+
